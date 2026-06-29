f1c234f4d571aeb4c2b54fd91f0baf1b5e99a374
jx06t <50313tjx06@gmail.com>
2026-06-29 23:17:11 +0800

    feat: initialize core firmware structure in main.c

--- firmware-rocket/Core/Src/main.c
+++ firmware-rocket/Core/Src/main.c
@@ -428,9 +428,9 @@ int main(void)
   MX_USART1_UART_Init();
   MX_TIM2_Init();
   MX_USB_DEVICE_Init();
-  MX_FATFS_Init();
   MX_SPI1_Init();
   MX_SPI2_Init();
+  MX_FATFS_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
